282881313f0f5e3fdc04cf1a6ee38704108b245a
Adams1991 <adams121@hotmail.co.uk>
2018-08-04 20:27:31 +0100

    input view fix implemented

--- src/views/input_view.js
+++ src/views/input_view.js
@@ -5,9 +5,9 @@ const InputView = function () {
 };
 
 InputView.prototype.bindEvents = function () {
-  const input = document.querySelector('#text');
-  input.addEventListener('input', (evt) => {
-    const inputtedText = evt.target.value;
+  const input = document.querySelector('#wordcounter-form');
+  input.addEventListener('submit', (event) => {
+    const inputtedText = event.target.text.value;
     PubSub.publish('InputView:text-inputted', inputtedText);
   });
 };
