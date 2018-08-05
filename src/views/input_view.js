df0a7601ac31361852fabf79d45960863aa1e9a6
Adams1991 <adams121@hotmail.co.uk>
2018-08-05 09:22:20 +0100

    added prevent default to input veiw to stop refresh

--- src/views/input_view.js
+++ src/views/input_view.js
@@ -7,6 +7,7 @@ const InputView = function () {
 InputView.prototype.bindEvents = function () {
   const input = document.querySelector('#wordcounter-form');
   input.addEventListener('submit', (event) => {
+      event.preventDefault();
     const inputtedText = event.target.text.value;
     PubSub.publish('InputView:text-inputted', inputtedText);
   });
